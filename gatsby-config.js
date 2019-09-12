a88a0e153be3586d46f8b8321c2080dc3744fc63
alphacrash <sudhansu.ace@gmail.com>
2019-09-13 01:27:58 +0530

    Added MDX support

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,14 @@ module.exports = {
         display: 'swap'
       }
     },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve(`./src/components/layout.js`),
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
